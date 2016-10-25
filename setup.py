2f6497686455a67d5d45fecdb0e8d89fe3a3c27b
Mike Vasiliou <mvasiliou94@gmail.com>
2016-10-24 21:36:27 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mailgun',
+      version='0.1',
+      description='Python Wrapper for the Mailgun API',
+      url='http://github.com/mvasiliou/mailgun',
+      author='Michael Vasiliou',
+      author_email='mvasiliou94@gmail.com',
+      license='MIT',
+      packages=['mailgun'],
+      zip_safe=False)
